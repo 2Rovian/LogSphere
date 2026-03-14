6d31099b062eda0b320a9d86520b90d0849a12cc
Ian Gurgel <iangurgel25@hotmail.com>
2026-03-14 13:03:40 -0300

    feat: working on MoviesComp. Currently has 3 states: search, create and mymovies

--- src/components/main_comp_folder/MoviesComp.tsx
+++ src/components/main_comp_folder/MoviesComp.tsx
@@ -1,14 +1,43 @@
+import { useState } from "react";
 
 function MoviesComp() {
+  const [mode, setMode] = useState<"search" | "create" | "mymovies">("search");
 
+  return (
+    <section className="flex flex-col gap-6 w-full">
 
-    return (
-        <section className="flex flex-col gap-8 w-full">
+      <div className="flex justify-between items-center">
 
-            <span>Movies Comp inside the moviespage</span>
+        <div className="flex gap-2">
+          <button
+            onClick={() => setMode("search")}
+            className={`${mode === "search" ? "primary-button" : "primary-outline"}`}
+          >
+            Search TMDB
+          </button>
 
-        </section>
-    );  
+          <button
+            onClick={() => setMode("create")}
+            className={`${mode === "create" ? "primary-button" : "primary-outline"}`}
+          >
+            Add manually
+          </button>
+        </div>
+
+        <button className={`${mode == "mymovies" ? "primary-button" : "primary-outline"}`}
+        onClick={() => setMode("mymovies")}
+        >
+          My movies
+        </button>
+
+      </div>
+
+      {mode === "search" && <div>Search component here</div>}
+      {mode === "create" && <div>Manual form here</div>}
+      {mode === "mymovies" && <div>All your movies here</div>}
+
+    </section>
+  );
 }
 
 export default MoviesComp;
